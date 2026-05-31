bfeb97d90f0e29873e8442a576e0359da5c873fa
2EZ4BLINK <blinkcode24@gmail.com>
2026-05-31 16:05:37 +0800

    update: added the list of products

--- index.js
+++ index.js
@@ -9,19 +9,58 @@ const url = require("url");
 // fs.writeFileSync("./txt/output.txt", textOut);
 // console.log("File written");
 
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{ProductName}/g, product.productName);
+  output = output.replace(/{ProductImage}/g, product.image);
+  output = output.replace(/{ProductPrice}/g, product.price);
+  output = output.replace(/{ProductFrom}/g, product.from);
+  output = output.replace(/{ProductNutrients}/g, product.nutrients);
+  output = output.replace(/{ProductQuantity}/g, product.quantity);
+  output = output.replace(/{ProductDescription}/g, product.description);
+  output = output.replace(/{ProductId}/g, product.id);
+
+  if (!product.organic) output = output.replace(/{NotOrganic}/g, "not-organic");
+
+  return output;
+};
+
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
+
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  "utf-8",
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8",
+);
+
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  "utf-8",
+);
 
 const server = http.createServer((req, res) => {
   const pathName = req.url;
 
-  if (pathName === "/") {
-    res.end("This is the home page");
+  if (pathName === "/" || pathName == "/overview") {
+    res.writeHead(200, {
+      "Content-type": "text/html",
+    });
+
+    const cardsHtml = dataObj.map((e) => replaceTemplate(tempCard, e)).join("");
+    const output = tempOverview.replace("{ProductCards}", cardsHtml);
+    res.end(output);
+
+    // API
   } else if (pathName === "/api") {
     res.writeHead(200, {
       "Content-type": "application/json",
     });
     res.end(data);
+
+    // Not found
   } else {
     res.writeHead(404, {
       "Content-type": "text/html",
